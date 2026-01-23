bd4f7ca2fbad69cd81ddc117b8cced256e78e49a
Yahya Alsabbagh <61578981+yahya-alsabbagh@users.noreply.github.com>
2026-01-23 12:46:04 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
   base: "/dajla-s-embrace/",
 });
+
 
 
 
